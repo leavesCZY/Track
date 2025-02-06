a689a52b2f5a9eeaf7c62ddfd66c8ceabacf40c8
leavesCZY <1990724437@qq.com>
2025-02-06 15:16:36 +0800

    optimize code

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Track 是一个适用于 Android 的字节码插桩库
 
 - View Click 双击防抖
 - Jetpack Compose Click 双击防抖
-- 收拢应用内所有的 Toast.show 方法。可用于解决在 Android 7.1 系统上 Toast 由于 WindowToken 失效从而导致应用崩溃的问题
+- 收拢应用内所有的 Toast.show 方法。可用于解决在 Android 7.1 系统上 Toast 由于 WindowToken
+  失效从而导致应用崩溃的问题
 - 收拢应用内所有的 Executors 线程池方法。可用于实现线程整治，统一应用内所有的线程池实例
 - 修改指定类的继承关系，将指定父类替换为另一个类
 - 修改指定字段的调用链，将指定字段替换为另一个字段
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,11 +13,11 @@ plugins {
 
 android {
     namespace = "github.leavesczy.track"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "github.leavesczy.track"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
