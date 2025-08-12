d92983aefbff840717f4f80aff8bf39572abb430
leavesCZY <1990724437@qq.com>
2025-08-12 21:26:52 +0800

    release:v1.1.3

--- track/build.gradle.kts
+++ track/build.gradle.kts
@@ -22,7 +22,7 @@ tasks.withType<KotlinCompile>().configureEach {
 }
 
 group = "io.github.leavesczy"
-version = "1.1.2"
+version = "1.1.3"
 
 gradlePlugin {
     website.set("https://github.com/leavesCZY/Track")
